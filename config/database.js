573d7d00712ca1e4f37af06c95e6c36a4d4da5ec
Muneeb Zia <luminite143@gmail.com>
2018-03-09 21:37:32 +0500

    sixth commit database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-database : 'mongodb://Muneeb147:Motu@7256@ds163418.mlab.com:63418/sparklecrowd',
+database : 'mongodb://monu:12345678@ds163418.mlab.com:63418/sparklecrowd',
 secret:'mysecret'
 
 }
